9720db7888b9c7ff6935f2a89a0c413de60610f3
Natalia Astafieva <asta.natalia31@gmail.com>
2022-01-19 00:02:00 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let num = Math.abs(n);
+  let str = num.toString();
+  let a = str.split('');
+  let s = a.reverse();
+  let r = s.join('');
+  return r;
 }
